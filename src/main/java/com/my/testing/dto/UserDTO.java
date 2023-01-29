e5c07ab290cb3a02a2cc0a74e679897e47737a8d
Dima Khelemendyk <khelemendykdmytro@gmail.com>
2023-01-29 12:26:01 +0200

    added annotation

--- src/main/java/com/my/testing/dto/UserDTO.java
+++ src/main/java/com/my/testing/dto/UserDTO.java
@@ -1,11 +1,14 @@
 package com.my.testing.dto;
 
 import lombok.*;
+
+import java.io.Serial;
 import java.io.Serializable;
 
 @Data
 @Builder
 public class UserDTO implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     private long id;
     private String email;
