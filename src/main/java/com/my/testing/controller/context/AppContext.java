6ad6e4d3309efeeb4f5620099ed97ef209718928
Dima Khelemendyk <khelemendykdmytro@gmail.com>
2023-02-06 19:34:50 +0200

    added question and answer services

--- src/main/java/com/my/testing/controller/context/AppContext.java
+++ src/main/java/com/my/testing/controller/context/AppContext.java
@@ -7,6 +7,8 @@ public class AppContext {
     private final ServiceFactory serviceFactory = ServiceFactory.getInstance();
     @Getter private final UserService userService = serviceFactory.getUserService();
     @Getter private final TestService testService = serviceFactory.getTestService();
+    @Getter private final QuestionService questionService = serviceFactory.getQuestionService();
+    @Getter private final AnswerService answerService = serviceFactory.getAnswerService();
 
     public static AppContext getAppContext() {
         return new AppContext();
